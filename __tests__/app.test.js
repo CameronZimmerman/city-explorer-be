36a3355a0791f6c989747145b079bd9eb37d81a8
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-26 13:20:54 -0800

    create munge location function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,14 +5,14 @@ let mungeUtils = require('../lib/api-munge-utils.js');
 test('returns location data in a presentable form', async() => {
 
   const expectation = {
-    formatted_query: 'Portland, Multnomah County, Oregon, USA',
+    formatted_query: 'Portland, Multnomah, Oregon, USA',
     latitude: '45.5202471',
     longitude: '-122.6741949'
   };
 
   const locationData = await request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_ACCESS_TOKEN}&q='portland'&format=json`);
 
-  let formattedLocation = mungeUtils.mungeLocationData(locationData.body[0]);
+  let formattedLocation = mungeUtils.mungeLocationData(locationData);
 
   expect(formattedLocation).toEqual(expectation);
 });
